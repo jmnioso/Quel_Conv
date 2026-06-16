be5835c829938be6a6f1f6bf771cdc1605c7c005
jmnioso <jmni.oso@gmail.com>
2026-06-16 20:16:18 +0200

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://afwqjdtsnbvzagczoitn.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_TbI8PKZeeet2-_CdSLEdJA_t6ZZCaVu';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmd3FqZHRzbmJ2emFnY3pvaXRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MDUwNTQsImV4cCI6MjA5NzA4MTA1NH0.7GkaSkrtBknT4wkdfKonbTZ1Lme2GajY-qF1mT_l5x0';
 
 // Ne pas modifier ci-dessous
 window.SUPABASE_URL = SUPABASE_URL;
